129f9d82b9e1bbaee67c7ad18bf4dcde6c0d58ef
Ismail Sariarslan <krookskala@gmail.com>
2025-06-25 11:28:26 +0200

    Define ArtGenre enum for artwork classification

---
+++ art-gallery/src/main/java/edu/pja/mas/s22899/artgallery/model/ArtGenre.java
@@ -0,0 +1,15 @@
+package edu.pja.mas.s22899.artgallery.model;
+
+public enum ArtGenre {
+    ABSTRACT,
+    REALISM,
+    IMPRESSIONISM,
+    EXPRESSIONISM,
+    SURREALISM,
+    CUBISM,
+    CONTEMPORARY,
+    BAROQUE,
+    RENAISSANCE,
+    FUTURISM,
+    NEOCLASSICISM
+}

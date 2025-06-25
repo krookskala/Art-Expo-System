0d107b59f1c8c32caeca6cd8028091316a31359e
Ismail Sariarslan <krookskala@gmail.com>
2025-06-25 12:15:45 +0200

    Add initial context-loading integration test

---
+++ art-gallery/src/test/java/edu/pja/mas/s22899/artgallery/ArtGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pja.mas.s22899.artgallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtGalleryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
